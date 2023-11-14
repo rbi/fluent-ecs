47e40c233cd41eccd1dc99b071411d48b459b217
Raik Bieniek <raik@bieniek-it.de>
2023-11-14 19:53:28 +0100

    Finish initial implementation of the kubernetes information mapper.

--- src/kubernetes.rs
+++ src/kubernetes.rs
@@ -18,9 +18,57 @@ fn convert(kubernetes: Kubernetes, json: &mut FluentBitJson) {
     orchestrator.type_val = Some("kubernetes".to_string());
     orchestrator.namespace = kubernetes.namespace_name;
 
+    let orchestrator_resource = orchestrator.resource();
+    orchestrator_resource.id = kubernetes.pod_id;
+    orchestrator_resource.name = kubernetes.pod_name;
+    orchestrator_resource.type_val = Some("Pod".to_string());
+    orchestrator_resource.label = kubernetes
+        .labels
+        .into_iter()
+        .map(|pair| {
+            format!(
+                "{}:{}",
+                pair.0,
+                match pair.1 {
+                    serde_json::Value::String(val) => val.to_string(),
+                    val => val.to_string(),
+                }
+            )
+        })
+        .collect();
+    if let Some(parent_type) = detect_resource_parent(&orchestrator_resource.label) {
+        orchestrator_resource.parent().type_val = Some(parent_type);
+    }
+
     // container
     let container = json.container();
     container.id = kubernetes.docker_id;
     container.name = kubernetes.container_name;
     container.image().name = kubernetes.container_image;
+
+    let container_hash = kubernetes
+        .container_hash
+        .map(extract_container_hash)
+        .flatten();
+    if let Some(hash) = container_hash {
+        container.image().hash().all = vec![hash]
+    }
+}
+
+fn extract_container_hash(hash: String) -> Option<String> {
+    let parts: Vec<&str> = hash.split("@").collect();
+    if parts.len() <= 1 {
+        return None;
+    }
+    parts.last().map(|last_part| last_part.to_string())
+}
+
+fn detect_resource_parent(label: &Vec<String>) -> Option<String> {
+    label.into_iter().find_map(|candidate| {
+        if candidate.starts_with("statefulset.kubernetes.io/pod-name:") {
+            Some("StatefulSet".to_string())
+        } else {
+            None
+        }
+    })
 }
--- src/model.rs
+++ src/model.rs
@@ -35,14 +35,40 @@ pub mod ecs {
     pub struct ContainerImage {
         #[serde(skip_serializing_if = "Option::is_none")]
         pub name: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub hash: Option<ContainerImageHash>,
 
         #[serde(flatten)]
         pub other: Value,
     }
+
     impl ContainerImage {
         pub fn new() -> Self {
             ContainerImage {
                 name: None,
+                hash: None,
+                other: Value::Null,
+            }
+        }
+
+        pub fn hash(&mut self) -> &mut ContainerImageHash {
+            self.hash.get_or_insert_with(|| ContainerImageHash::new())
+        }
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub struct ContainerImageHash {
+        #[serde(skip_serializing_if = "Vec::is_empty")]
+        pub all: Vec<String>,
+
+        #[serde(flatten)]
+        pub other: Value,
+    }
+
+    impl ContainerImageHash {
+        pub fn new() -> Self {
+            ContainerImageHash {
+                all: Vec::new(),
                 other: Value::Null,
             }
         }
@@ -73,6 +99,8 @@ pub mod ecs {
         pub type_val: Option<String>,
         #[serde(skip_serializing_if = "Option::is_none")]
         pub namespace: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub resource: Option<OrchestratorResource>,
 
         #[serde(flatten)]
         pub other: Value,
@@ -83,6 +111,64 @@ pub mod ecs {
             Orchestrator {
                 type_val: None,
                 namespace: None,
+                resource: None,
+                other: Value::Null,
+            }
+        }
+
+        pub fn resource(&mut self) -> &mut OrchestratorResource {
+            self.resource.get_or_insert_with(|| OrchestratorResource::new())
+        }
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub struct OrchestratorResource {
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub id: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub name: Option<String>,
+        #[serde(rename = "type")]
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub type_val: Option<String>,
+        #[serde(skip_serializing_if = "Vec::is_empty")]
+        pub label: Vec<String>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub parent: Option<OrchestratorResourceParent>,
+
+        #[serde(flatten)]
+        pub other: Value,
+    }
+
+    impl OrchestratorResource {
+        pub fn new() -> Self {
+            OrchestratorResource {
+                id: None,
+                name: None,
+                type_val: None,
+                label: Vec::new(),
+                parent: None,
+                other: Value::Null,
+            }
+        }
+
+        pub fn parent(&mut self) -> &mut OrchestratorResourceParent {
+            self.parent.get_or_insert_with(|| OrchestratorResourceParent::new())
+        }
+    }
+    #[derive(Serialize, Deserialize)]
+    pub struct OrchestratorResourceParent {
+        #[serde(rename = "type")]
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub type_val: Option<String>,
+
+        #[serde(flatten)]
+        pub other: Value,
+    }
+
+    impl OrchestratorResourceParent {
+        pub fn new() -> Self {
+            OrchestratorResourceParent {
+                type_val: None,
                 other: Value::Null,
             }
         }
@@ -91,7 +177,7 @@ pub mod ecs {
 
 pub mod fluentbit {
     use serde_derive::{Deserialize, Serialize};
-    use serde_json::Value;
+    use serde_json::{Map, Value};
 
     #[derive(Serialize, Deserialize)]
     pub struct Kubernetes {
@@ -111,6 +197,8 @@ pub mod fluentbit {
         pub pod_id: Option<String>,
         #[serde(skip_serializing_if = "Option::is_none")]
         pub pod_name: Option<String>,
+        #[serde(skip_serializing_if = "Map::is_empty")]
+        pub labels: Map<String, Value>,
 
         #[serde(flatten)]
         pub other: Value,
